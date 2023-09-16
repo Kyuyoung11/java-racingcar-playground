b838fffaf2f9bf606c3330ef281915e768aa88f6
Kyuyoung11 <58154672+Kyuyoung11@users.noreply.github.com>
2023-09-16 23:52:35 +0900

    문자열 덧셈 계산기 - 쉼표,콜론

--- src/test/java/stringCalculatorTest/StringCalculatorTest.java
+++ src/test/java/stringCalculatorTest/StringCalculatorTest.java
@@ -1,11 +1,46 @@
 package stringCalculatorTest;
 
 import org.junit.jupiter.api.Test;
+import stringAddCalculator.StringCalculator;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StringCalculatorTest {
     @Test
-    public void splitString() {
+    public void splitStringNullOrEmpty() {
+        int sum = StringCalculator.splitAndSum(null);
+        assertEquals(sum,0);
 
+        sum = StringCalculator.splitAndSum("");
+        assertEquals(sum, 0);
+    }
+
+    @Test
+    public void splitStringNum1() {
+        int sum = StringCalculator.splitAndSum("1");
+        assertEquals(sum, 1);
+    }
+
+    @Test
+    public void splitAndSumString_쉼표() {
+        int sum = StringCalculator.splitAndSum("1,2");
+        assertEquals(sum, 3);
+
+    }
+
+    @Test
+    public void splitAndSumString_쉼표또는콜론() {
+        int sum = StringCalculator.splitAndSum("1,2:3");
+        assertEquals(sum, 6);
+    }
+
+    @Test
+    public void splitAndSumString_Custom() {
+        int sum = StringCalculator.splitAndSum("//;\n1;2;3");
+        assertEquals(sum, 6);
     }
 
 
