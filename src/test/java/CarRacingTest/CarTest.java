7fc5cfc0706c1e28a641c63ea16e3dd0c3d7526a
Kyuyoung11 <kyuyoung.lee@bankwareglobal.com>
2023-09-20 12:49:12 +0900

    car print 기능 추가

--- src/test/java/CarRacingTest/CarTest.java
+++ src/test/java/CarRacingTest/CarTest.java
@@ -4,16 +4,31 @@ import CarRacing.object.Car;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CarTest {
     Car car;
 
     @BeforeEach
     void setUp() {
-        car = new Car();
+        car = new Car("test");
+    }
+    @Test
+    void moveCarTest() {
+        car.moveCar(1);
+        assertEquals(car.getDistance(),1);
+
+        car.moveCar(4);
+        assertEquals(car.getDistance(),5);
     }
 
-    private void makeTestCar() {
-        car = new Car("test");
+    @Test
+    void toStringTest() {
+        car.moveCar(3);
+        assertEquals(car.toString(),"test : ---");
+        car.moveCar(5);
+        assertEquals(car.toString(), "test : --------");
+
     }
 
 
