6b578b3313a37fd9a02668024f02fb1a75bb2831
Kyuyoung11 <58154672+Kyuyoung11@users.noreply.github.com>
2023-09-26 00:36:37 +0900

    (feat) validate 로직 추가

--- src/test/java/CarRacingTest/CarRacingTest.java
+++ src/test/java/CarRacingTest/CarRacingTest.java
@@ -4,7 +4,9 @@ import CarRacing.object.Cars;
 import CarRacing.service.CarRacing;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import stringAddCalculator.StringCalculator;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -17,7 +19,7 @@ public class CarRacingTest {
     }
 
     @Test
-    void makeCarsTest() {
+    void makeCarsTest() throws Exception {
         Cars testCars = carRacing.makeCars("1,a,zzz");
         assertEquals(testCars.getCars().get(0).getName(), "1");
         assertEquals(testCars.getCars().get(1).getName(), "a");
@@ -25,9 +27,16 @@ public class CarRacingTest {
     }
 
     @Test
-    void moveCarsTest() {
+    void moveCarsTest() throws RuntimeException {
         Cars testCars = carRacing.makeCars("1,a,zzz");
         carRacing.moveCars(testCars);
+    }
+
+    @Test
+    void carNameExceptionTest() throws RuntimeException {
+
+        assertThatThrownBy(() -> carRacing.makeCars("aaaaaaa,1,2"))
+                .isInstanceOf(RuntimeException.class);
     }
 
 
