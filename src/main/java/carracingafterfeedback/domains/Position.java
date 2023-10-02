2223e51fb6180c1c7e2c97728b2759632a2e276e
Kyuyoung11 <58154672+Kyuyoung11@users.noreply.github.com>
2023-10-02 23:44:04 +0900

    [feat] Car 생성

---
+++ src/main/java/carracingafterfeedback/domains/Position.java
@@ -0,0 +1,13 @@
+package carracingafterfeedback.domains;
+
+public class Position {
+    private final int position;
+
+    public Position(int position) {
+        this.position = position;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+}
