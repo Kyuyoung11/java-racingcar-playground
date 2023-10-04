cdf4cc34a870b77541634fea68fbcbc5019bac8e
Kyuyoung11 <58154672+Kyuyoung11@users.noreply.github.com>
2023-10-05 00:01:27 +0900

    [feat] 시도횟수 입력

--- src/main/java/carracingafterfeedback/view/RacingInputView.java
+++ src/main/java/carracingafterfeedback/view/RacingInputView.java
@@ -9,4 +9,9 @@ public class RacingInputView {
         return sc.nextLine();
     }
 
+    public static int getTryCount() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("시도할 회수는 몇회인가요?");
+        return sc.nextInt();
+    }
 }
